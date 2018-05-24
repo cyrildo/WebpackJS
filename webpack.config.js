dc9e7f5c62788dd7bc6b890ce5bf3ad64dbf31bb
Cyril Do <cyrildo@yahoo.fr>
2018-05-24 19:31:36 +0200

    f***ing commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './js/index.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS
+            ]
+        }]
+    }
+}
